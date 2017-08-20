db874ece0e3faaec7472ed4968f93f146051c683
Ldlood <xz95@outlokk.com>
2017-08-20 14:10:26 +0800

    优化显示

--- src/main/java/com/ldlood/Service/WebSocket.java
+++ src/main/java/com/ldlood/Service/WebSocket.java
@@ -1,5 +1,7 @@
 package com.ldlood.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ldlood.VO.MessageVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -22,11 +24,28 @@ public class WebSocket {
 
     private static CopyOnWriteArraySet<WebSocket> webSockets = new CopyOnWriteArraySet<>();
 
+    private MessageVO messageVO = new MessageVO();
+
     @OnOpen
     public void onOpen(Session session) {
         this.session = session;
         webSockets.add(this);
-        this.sendMessage("有新的进入");
+
+
+        messageVO.setType(1);
+        messageVO.setUserNum(webSockets.size());
+        messageVO.setMessage("有新的连接");
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String Json = "";
+        try {
+            Json = mapper.writeValueAsString(messageVO);
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+        }
+
+        this.sendMessage(Json);
         log.info("【websocket消息】有新的连接, 总数:{}", webSockets.size());
     }
 
@@ -34,13 +53,44 @@ public class WebSocket {
     @OnClose
     public void onClose() {
         webSockets.remove(this);
-        this.sendMessage("有用户离开");
+
+        messageVO.setType(2);
+        messageVO.setUserNum(webSockets.size());
+        messageVO.setMessage("有用户离开");
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String Json = "";
+        try {
+            Json = mapper.writeValueAsString(messageVO);
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+        }
+
+        this.sendMessage(Json);
+
+
         log.info("【websocket消息】连接断开, 总数:{}", webSockets.size());
     }
 
     @OnMessage
     public void onMessage(String message) {
-        this.sendMessage(message);
+
+        messageVO.setType(3);
+        messageVO.setUserNum(webSockets.size());
+        messageVO.setMessage(message);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        String Json = "";
+        try {
+            Json = mapper.writeValueAsString(messageVO);
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+        }
+
+        this.sendMessage(Json);
+
         log.info("【websocket消息】收到客户端发来的消息:{}", message);
     }
 
